7c04357f89b6b867be34f2da4cb618897c2db190
jprochazk <1665677+jprochazk@users.noreply.github.com>
2024-07-09 14:26:24 +0200

    update build

--- frontend/gradio.config.js
+++ frontend/gradio.config.js
@@ -1,9 +1,24 @@
 import tailwindcss from "@tailwindcss/vite";
 import wasm from "vite-plugin-wasm";
 
+// `@rerun-io/web-viewer` uses `new URL("./re_viewer_bg.wasm", import.meta.url)` since 0.17,
+// which does not play well with `vite dev`: https://github.com/rerun-io/rerun/issues/6815
+// we need to patch the config, but `gradio` does not let us directly set the `optimize` option.
+/** @type {() => import("vite").Plugin} */
+const hack = () => ({
+  config() {
+    return {
+      optimizeDeps: {
+        exclude: process.env.NODE_ENV === "production" ? [] : ["@rerun-io/web-viewer"],
+      },
+    };
+  },
+});
+
 export default {
-  plugins: [wasm(), tailwindcss()],
+  plugins: [wasm(), tailwindcss(), hack()],
   svelte: {
     preprocess: [],
   },
 };
+
